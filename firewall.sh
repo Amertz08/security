9d938e88a0a838c3108e414f894989887ac0943c
Adam Mertz <adammertz@gmail.com>
2017-09-17 09:17:22 -0500

    Added more functionality to firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -3,6 +3,11 @@
 # Helps manage UFW
 # Tested:
 #   Ubuntu 16.04
+#
+# To make executable:
+#   1) chmod +x firewall.sh
+#   2) mv firewall.sh /usr/local/bin/fw
+
 
 init () {
     checkinstall
@@ -11,16 +16,44 @@ init () {
     ufw default deny incoming
     allow ssh
     ufw enable
+    echo "Firewall enabled"
 }
 
 checkinstall () {
     command -v ufw >/dev/null 2>&1 || { echo >&2 "UFW not installed. Installing..."; apt update && apt install -y ufw; }
 }
 
-allow () {
-    ufw allow $1
+cycle () {
+    ufw disable
+    ufw enable
+    echo "Firewall restarted"
 }
 
+status () {
+    ufw status numbered
+}
 
-init
-echo "Done"
+delete () {
+    echo "Deleting " $@
+    ufw delete $@
+}
+
+allow () {
+    echo "Adding allow for " $@
+    ufw allow $@
+}
+
+deny () {
+    echo "Adding deny for " $@
+    ufw deny $@
+}
+
+aip () {
+    echo "Allowing from " $@
+    ufw allow from $@
+}
+
+dip () {
+    echo "Denying from " $@
+    ufw deny from $@
+}
