6624cd8e847b9f4bee38eda4dd46171150dc3263
Adam Mertz <adammertz@gmail.com>
2017-09-16 21:46:08 -0500

    added commands to extract tar and run install

--- install-ossec.sh
+++ install-ossec.sh
@@ -3,6 +3,8 @@
 echo "Install OSSEC"
 wget -U ossec https://bintray.com/artifact/download/ossec/ossec-hids/ossec-hids-2.8.3.tar.gz
 wget -U ossec https://raw.githubusercontent.com/ossec/ossec-docs/master/docs/whatsnew/checksums/2.8.3/ossec-hids-2.8.3.tar.gz.sha256
-cat ossec-hids-2.8.3.tar.gz.sha256
 sha256sum -C  ossec-hids-2.8.3.tar.gz.sha256 ossec-hids-2.8.3.tar.gz
+tar -zxvf ossec-hids-*.tar.gz
+rm ossec-hids-*.tar.gz
+source ./ossec-hids-*/install.sh
 echo "Done"
