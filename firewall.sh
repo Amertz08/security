73a73198fc8a4f9917652dd0512c3fdee997ae3b
Adam Mertz <adammertz@gmail.com>
2017-09-17 01:01:51 -0500

    Removed ufw install from Dockerfile and created function to install in firewall script.

--- firewall.sh
+++ firewall.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
 
+# Helps manage UFW
+# Tested:
+#   Ubuntu 16.04
+
 init () {
+    checkinstall
     echo "Initial config"
     ufw default allow outgoing
     ufw default deny incoming
     allow ssh
     ufw enable
+}
+
+checkinstall () {
+    command -v ufw >/dev/null 2>&1 || { echo >&2 "UFW not installed. Installing..."; apt update && apt install -y ufw; }
 }
 
 allow () {
