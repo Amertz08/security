e3ea0a7764fb263af4aefdbdea92d5a51fc6182e
adam <adammertz@gmail.com>
2017-09-16 21:37:01 -0500

    Initial commit.

---
+++ install-ossec.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+echo "Install OSSEC"
+wget -U ossec https://bintray.com/artifact/download/ossec/ossec-hids/ossec-hids-2.8.3.tar.gz
+wget -U ossec https://raw.githubusercontent.com/ossec/ossec-docs/master/docs/whatsnew/checksums/2.8.3/ossec-hids-2.8.3.tar.gz.sha256
+cat ossec-hids-2.8.3.tar.gz.sha256
+sha256sum -C  ossec-hids-2.8.3.tar.gz.sha256 ossec-hids-2.8.3.tar.gz
+echo "Done"
