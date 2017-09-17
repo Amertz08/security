5690087273c60b5dd9c7ebb1711228c8a40426b2
Adam Mertz <adammertz@gmail.com>
2017-09-17 01:00:30 -0500

    Added comments to denote OS tested on.

--- ossec-download.sh
+++ ossec-download.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
+# Downloads OSSECS
+# Tested:
+#   Ubuntu 16.04
+
 echo "Download OSSEC"
 mkdir /opt/ossec
 wget -U ossec https://bintray.com/artifact/download/ossec/ossec-hids/ossec-hids-2.8.3.tar.gz
 wget -U ossec https://raw.githubusercontent.com/ossec/ossec-docs/master/docs/whatsnew/checksums/2.8.3/ossec-hids-2.8.3.tar.gz.sha256
 
 # Verify proper download
-check=$(sha256sum -c  ossec-hids-2.8.3.tar.gz.sha256 ossec-hids-2.8.3.tar.gz)
+check=$(sha256sum -c  ossec-hids-2.8.3.tar.gz.sha256 ossec-hids-2.8.3.tar.gz | grep "OK")
 if [[ $check != *"OK"* ]]; then
     echo "sha256 check failed!"
     rm ossec-hids-*.tar.gz
