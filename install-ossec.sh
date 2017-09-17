7df82e7a015139d1cc6738d9e97024c13a61c539
Adam Mertz <adammertz@gmail.com>
2017-09-16 22:20:35 -0500

    changed directory tar is output to

--- install-ossec.sh
+++ install-ossec.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 echo "Install OSSEC"
+mkdir /opt/ossec
 wget -U ossec https://bintray.com/artifact/download/ossec/ossec-hids/ossec-hids-2.8.3.tar.gz
 wget -U ossec https://raw.githubusercontent.com/ossec/ossec-docs/master/docs/whatsnew/checksums/2.8.3/ossec-hids-2.8.3.tar.gz.sha256
 sha256sum -C  ossec-hids-2.8.3.tar.gz.sha256 ossec-hids-2.8.3.tar.gz
-tar -zxvf ossec-hids-*.tar.gz
+tar -zxvf ossec-hids-*.tar.gz -C /opt/ossec --strip-components=1
 rm ossec-hids-*.tar.gz
-source ./ossec-hids-*/install.sh
 echo "Done"
