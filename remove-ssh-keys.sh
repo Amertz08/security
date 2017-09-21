fd449870e4111c62b7b0b5a8f85aef32b1036112
Adam Mertz <adammertz@gmail.com>
2017-09-21 17:04:41 -0500

    Programmatically remove all key files

--- remove-ssh-keys.sh
+++ remove-ssh-keys.sh
@@ -4,14 +4,12 @@
 # Tested:
 #   Ubuntu 16.04
 
-KEY_FILE=/home/$USER/.ssh/authorized_keys
-
-echo "Key file:" $KEY_FILE
-
-if [ ! -f $KEY_FILE ]; then
-    echo "authorized_keys file not found"
-else
-    rm $KEY_FILE
-    touch $KEY_FILE
-    echo "Keys cleared"
-fi
+for X in $(cut -f6 -d ':' /etc/passwd | sort | uniq); do
+    if [ -s "${X}/.ssh/authorized_keys" ]; then
+        echo "### ${X}: "
+        rm ${X}/.ssh/authorized_keys
+        touch ${X}/.ssh/authorized_keys
+    else
+        echo "$X has no key file"
+    fi
+done
